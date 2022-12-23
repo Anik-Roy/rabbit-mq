3d7adfff4fd786871d0e33f77358510691f2ce6c
Anik Roy Gourab <anik96lu@gmail.com>
2022-12-23 19:14:34 +0600

    users array added

--- routes/app/lib/publishMessageToQueueMiddleware.js
+++ routes/app/lib/publishMessageToQueueMiddleware.js
@@ -8,6 +8,18 @@ const queue = 'notification';
  * @returns {Promise<Response>}
  */
 async function storeMessageToQueueMiddleware(req, res) {
+  const users = [
+    {
+      platform_user_id: '633fd8ea2dbcfe23c7fa63ef',
+    },
+    {
+      platform_user_id: '630dce0ed5ee9f1651de4ac6',
+    },
+    {
+      platform_user_id: '633e96591da359080e0f1308',
+    },
+  ];
+
   try {
     amqp.connect('amqp://localhost', function (error0, connection) {
       if (error0) {
@@ -17,21 +29,31 @@ async function storeMessageToQueueMiddleware(req, res) {
         if (error1) {
           throw error1;
         }
-        
+
         const data = req.body;
 
         channel.assertQueue(queue, {
-          durable: false
+          durable: false,
         });
-    
-        Array.from(new Array(5)).map((_, i) => {
-          channel.sendToQueue(queue, Buffer.from(JSON.stringify({ index: i, ...data})));
-          console.log('message published to queue~!', i);
+
+        // Array.from(new Array(5)).map((_, i) => {
+        //   channel.sendToQueue(
+        //     queue,
+        //     Buffer.from(JSON.stringify({ index: i, ...data }))
+        //   );
+        //   console.log('message published to queue~!', i);
+        // });
+        users.map((user, i) => {
+          channel.sendToQueue(
+            queue,
+            Buffer.from(JSON.stringify({ index: i, ...data, userId: user.platform_user_id }))
+          );
+          console.log('message published to queue~!', user.platform_user_id);
         });
       });
-      setTimeout(function() {
+      setTimeout(function () {
         connection.close();
-    }, 500);
+      }, 500);
     });
     res.send('message published!');
   } catch (error) {
